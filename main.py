a667a16d0d25075195d34e5b85309be19e7bb620
ldefeo <ldefeo@fi.uba.ar>
2023-10-07 15:45:43 -0300

    modification of main

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ def main():
     customers_n_tuples = [([10,2,3,3],2),([5,3,4],1),([2,3,10],2)]
     for tuple in range(len(customers_n_tuples)):
         time_request = self_service(customers_n_tuples[tuple][0], customers_n_tuples[tuple][1])
-        print("The total time required to register is: ",time_request)
+        print("The total time required for the tuple ({},{}) to register is: {}".format(customers_n_tuples[tuple][0],customers_n_tuples[tuple][1],time_request))
         
 main()
